4a0df0e9fceff933fb376a0bcede34251c74328b
Chiranjeev Goel <77891118+Chiranjeev-goel@users.noreply.github.com>
2023-12-31 09:03:21 +0000

    variable done

---
+++ 01_basics/variable.js
@@ -0,0 +1,15 @@
+const accountId = 12456
+let accountEmail = "monu@google.com"
+var accountPassword = "1234"
+accountCity = "delhi"
+
+let accountState;
+// accountId = 2 //not allow
+
+accountEmail = "chi@s.com"
+accountPassword = "12345"
+accountCity = "faridabad"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountCity, accountPassword, accountState])
